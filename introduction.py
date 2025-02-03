760ec1d4309f28004c7ce7d49fc364ed850e1b4f
kirumbastacy25l <minniebooh7@gmail.com>
2025-02-03 13:13:51 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,8 @@
+
+
+#Print is a method used to display a value
+
+print("Stacy")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,28 @@
+#Variable is a container used to store a value
+
+number = 10
+
+firstname = "Stacy"
+lastname = "Kirumba"
+
+
+language = "Python"
+a = 10
+b = 15
+
+word = "The sum"
+
+print(number)
+
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+
+print(language, "is a web programming language")
+
+print(a + b)
+
+print(word,"of" " 10 and 15 is ",a + b)
+
+
+
